3c9efe58415b038010e060ed1de6f42c527caf51
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-04-08 22:01:30 -0500

    basic demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 600, "test");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("TEST", 100, 100, 20 , RED);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }

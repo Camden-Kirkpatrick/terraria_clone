8544795246574eb0c9a4bbc3e031b9fdcafe7cc7
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-04-12 17:03:32 -0500

    added code for imgui demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 #include <raylib.h>
 #include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 600, "test");
 
+	rlImGuiSetup(true);
+
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		//DrawText("TEST", 100, 100, 20 , RED);
+		rlImGuiBegin();
 
-		DrawRectangle(75, 75, 100, 100, { 0, 255, 0, 255 });
-		DrawRectangle(50, 50, 100, 100, { 255, 0, 0, 127 });
-	
+		ImGui::Begin("test");
+
+		ImGui::Text("hello");
+		ImGui::Button("button");
+
+		ImGui::End();
+
+
+		DrawText("TEST", 100, 100, 20 , RED);
+
+		DrawRectangle(175, 175, 100, 100, { 0, 255, 0, 255 });
+		DrawRectangle(150, 150, 100, 100, { 255, 0, 0, 127 });
+
+		rlImGuiEnd();
 		EndDrawing();
 	}
 
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;

9ca7528bdfb01e2128758141dbbaec9f07399e83
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-04-26 12:38:23 -0500

    improved random world generation

--- src/gameLayer/worldGenerator.cpp
+++ src/gameLayer/worldGenerator.cpp
@@ -3,108 +3,183 @@
 
 void generateWorld(GameMap& gameMap, int seed)
 {
-    const int w = 900;
-    const int h = 500;
+    const int WIDTH = 900;
+    const int HEIGHT = 500;
 
-    gameMap.create(w, h);
+    gameMap.create(WIDTH, HEIGHT);
 
+    // Use a random number generator populated with a seed
     std::ranlux24_base rng(seed);
 
-    int dirtDirectionTimer = getRandomInt(rng, 5, 50);
-    int dirtDirection = getRandomInt(rng, -2, 2);
+    const int MIN_TIME = 5;
+    const int MAX_TIME = 50;
+    // Y coordinate bounds that clamp where the dirt/stone layer boundaries can settle (y=0 is the top of the world)
+    const int MIN_DIRT_HEIGHT = 50;
+    const int MAX_DIRT_HEIGHT = 90;
+    const int MIN_STONE_HEIGHT = 60;
+    const int MAX_STONE_HEIGHT = 120;
+    const int GOLD_THRESHOLD = 30;
+    const float GOLD_CHANCE = 0.01f;
+   
+    // A timer set with a random value. When the timer reaches 0, a new direction is selected.
+    int dirtDirectionTimer = getRandomInt(rng, MIN_TIME, MAX_TIME);
 
-    int stoneDirectionTimer = getRandomInt(rng, 5, 50);
-    int stoneDirection = getRandomInt(rng, -2, 2);
+    enum
+    {
+        STEEP_DECLINE = -3,
+        DECLINE = -2,
+        MILD_DECLINE = -1,
+        FLAT_SURFACE = 0,
+        MILD_INCLINE = 1,
+        INCLINE = 2,
+        STEEP_INCLINE = 3
+    };
 
+    // Different directions that dirt can be placed in
+    int dirtDirection = getRandomInt(rng, STEEP_DECLINE, STEEP_INCLINE);
+
+    int stoneDirectionTimer = getRandomInt(rng, MIN_TIME, MAX_TIME);
+    int stoneDirection = getRandomInt(rng, STEEP_DECLINE, STEEP_INCLINE);
+
+    // Thresholds for when the block type changes
     int dirtHeight = 70;
     int stoneHeight = 90;
 
-    for (int x = 0; x < w; x++)
+    // Go through every block in the map
+    for (int x = 0; x < WIDTH; x++)
     {
+        // Subtract one from the timer for each horizontal distance made
         dirtDirectionTimer--;
-        if (dirtDirectionTimer <= 0)
+        // Reset the timer and pick a new direction
+        if (dirtDirectionTimer == 0)
         {
-            dirtDirectionTimer = getRandomInt(rng, 5, 50);
-            dirtDirection = getRandomInt(rng, -2, 2);
+            dirtDirectionTimer = getRandomInt(rng, MIN_TIME, MAX_TIME);
+            dirtDirection = getRandomInt(rng, STEEP_DECLINE, STEEP_INCLINE);
         }
 
-        if (dirtDirection == -1)
+        // Change dirtHeight based off of the dirtDirection
+        switch (dirtDirection)
         {
-            if (getRandomChance(rng, 0.25f))
-                dirtHeight--;
-        }
-        else if (dirtDirection == -2)
-        {
-            if (getRandomChance(rng, 0.25f))
-                dirtHeight--;
-            if (getRandomChance(rng, 0.25f))
-                dirtHeight--;
-        }
-        else if (dirtDirection == 1)
-        { 
+        case STEEP_DECLINE:
+            for (int i = 0; i < 3; i++)
+                if (getRandomChance(rng, 0.25f))
+                    dirtHeight++;
+            break;
+
+        case DECLINE:
+            for (int i = 0; i < 2; i++)
+                if (getRandomChance(rng, 0.25f))
+                    dirtHeight++;
+            break;
+
+        case MILD_DECLINE:
             if (getRandomChance(rng, 0.25f))
                 dirtHeight++;
-        }
-        else if (dirtDirection == 2)
-        {
+            break;
+
+        case MILD_INCLINE:
             if (getRandomChance(rng, 0.25f))
-                dirtHeight++;
-            if (getRandomChance(rng, 0.25f))
-                dirtHeight++;
+                dirtHeight--;
+            break;
+
+        case INCLINE:
+            for (int i = 0; i < 2; i++)
+                if (getRandomChance(rng, 0.25f))
+                    dirtHeight--;
+            break;
+
+        case STEEP_INCLINE:
+            for (int i = 0; i < 3; i++)
+                if (getRandomChance(rng, 0.25f))
+                    dirtHeight--;
+            break;
+
+        default:
+            break;
         }
 
-        if (dirtHeight < 50) dirtHeight = 50;
-        if (dirtHeight > 90) dirtHeight = 90;
+        // dirtHeight must be within this range
+        if (dirtHeight < MIN_DIRT_HEIGHT) dirtHeight = MIN_DIRT_HEIGHT;
+        if (dirtHeight > MAX_DIRT_HEIGHT) dirtHeight = MAX_DIRT_HEIGHT;
         
 
+        // Same exact code, but for stone instead
         stoneDirectionTimer--;
-        if (stoneDirectionTimer <= 0)
+        if (stoneDirectionTimer == 0)
         {
-            stoneDirectionTimer = getRandomInt(rng, 5, 50);
-            stoneDirection = getRandomInt(rng, -2, 2);
+            stoneDirectionTimer = getRandomInt(rng, MIN_TIME, MAX_TIME);
+            stoneDirection = getRandomInt(rng, STEEP_DECLINE, STEEP_INCLINE);
         }
 
-        if (stoneDirection == -1)
+        switch (stoneDirection)
         {
-            if (getRandomChance(rng, 0.25f))
-                stoneHeight--;
-        }
-        else if (stoneDirection == -2)
-        {
-            if (getRandomChance(rng, 0.25f))
-                stoneHeight--;
-            if (getRandomChance(rng, 0.25f))
-                stoneHeight--;
-        }
-        else if (stoneDirection == 1)
-        {
+        case STEEP_DECLINE:
+            for (int i = 0; i < 3; i++)
+                if (getRandomChance(rng, 0.25f))
+                    stoneHeight++;
+            break;
+
+        case DECLINE:
+            for (int i = 0; i < 2; i++)
+                if (getRandomChance(rng, 0.25f))
+                    stoneHeight++;
+            break;
+
+        case MILD_DECLINE:
             if (getRandomChance(rng, 0.25f))
                 stoneHeight++;
-        }
-        else if (stoneDirection == 2)
-        {
+            break;
+
+        case MILD_INCLINE:
             if (getRandomChance(rng, 0.25f))
-                stoneHeight++;
-            if (getRandomChance(rng, 0.25f))
-                stoneHeight++;
+                stoneHeight--;
+            break;
+
+        case INCLINE:
+            for (int i = 0; i < 2; i++)
+                if (getRandomChance(rng, 0.25f))
+                    stoneHeight--;
+            break;
+
+        case STEEP_INCLINE:
+            for (int i = 0; i < 3; i++)
+                if (getRandomChance(rng, 0.25f))
+                    stoneHeight--;
+            break;
+        default:
+            break;
         }
 
-        if (stoneHeight < 60) stoneHeight = 60;
-        if (stoneHeight > 120) stoneHeight = 120;
+        if (stoneHeight < MIN_STONE_HEIGHT) stoneHeight = MIN_STONE_HEIGHT;
+        if (stoneHeight > MAX_STONE_HEIGHT) stoneHeight = MAX_STONE_HEIGHT;
 
-        for (int y = 0; y < h; y++)
+        // Set the block type based on the heights
+        for (int y = 0; y < HEIGHT; y++)
         {
             Block b;
 
-            if (y > dirtHeight) b.type = Block::dirt;
+            // When y is above the stoneHight threshold, stone can generate
+            if (y > stoneHeight)
+            {
+                // Gold can generate further down in the stone layer
+                if (y > stoneHeight + GOLD_THRESHOLD)
+                {
+                    if (getRandomChance(rng, GOLD_CHANCE))
+                        b.type = Block::gold;
+                    else
+                        b.type = Block::stone;
+                }
+                // Not deep enough to generate gold 
+                else
+                    b.type = Block::stone; 
+            }
 
-            if (y == dirtHeight) b.type = Block::grassBlock;
-
-            if (y > stoneHeight) b.type = Block::stone;
+            // When y is above the dirtHeight threshold, dirt can generate
+            else if (y > dirtHeight)   b.type = Block::dirt;
+            // When y is exactly equal to the dirtHeight threshold, grass generates
+            else if (y == dirtHeight)  b.type = Block::grassBlock;
 
             gameMap.getBlockUnsafe(x, y) = b;
         }
     }
-
-    
 }

abc051fb67061d596dbaee0730f6f52b1f51b484
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-05-06 00:39:47 -0500

    added WorldNoise struct

--- src/gameLayer/worldGenerator.hpp
+++ src/gameLayer/worldGenerator.hpp
@@ -1,4 +1,22 @@
 #pragma once
 #include "gameMap.hpp"
 
+struct WorldNoise
+{
+	int dirtMountainOctaves = 1;
+	float dirtMountainFrequency = 0.02f;
+	int stoneMountainOctaves = 4;
+	float stoneMountainFrequency = 0.01f;
+
+	int dirtPlainOctaves = 1;
+	float dirtPlainFrequency = 0.0025f;
+	int stonePlainOctaves = 1;
+	float stonePlainFrequency = 0.005f;
+
+	int biomeOctaves = 1;
+	float biomeFrequency = 0.00025f;
+};
+
+extern WorldNoise worldNoise;
+
 void generateWorld(GameMap& gameMap, const int WIDTH = 900, const int HEIGHT = 500, int seed = 2112);

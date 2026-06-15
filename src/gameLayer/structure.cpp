73d0f65538d6970c36f195b508b4ad278aa7810e
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-06-14 21:28:39 -0500

    implemented 'copy from map' functionality

--- src/gameLayer/structure.cpp
+++ src/gameLayer/structure.cpp
@@ -37,3 +37,50 @@ Block* Structure::getBlockSafe(int x, int y)
 
 	return &structureBlocks[w * y + x];
 }
+
+// start and end define the area being selected
+void Structure::copyFromMap(GameMap& map, Vector2 start, Vector2 end)
+{
+	// Make sure the data passed in is valid
+	if (end.x >= map.w)
+		end.x = map.w - 1;
+	if (end.y >= map.h)
+		end.y = map.h - 1;
+
+	if (end.x < 0)
+		end.x = 0;
+	if (end.y < 0)
+		end.y = 0;
+
+	if (start.x < 0)
+		start.x = 0;
+	if (start.y < 0)
+		start.y = 0;
+
+	if (start.x > end.x)
+		std::swap(start.x, end.x);
+	if (start.y > end.y)
+		std::swap(start.y, end.y);
+
+	Vector2 size = Vector2{ end.x - start.x + 1, end.y - start.y + 1 };
+
+	if (size.x > map.w)
+		return;
+	if (size.y > map.h)
+		return;
+
+	// Allocate memory for the structure
+	create(size.x, size.y);
+
+	// Copy the blocks from the map into the structure
+	for (int y = 0; y < size.y; y++)
+	{
+		for (int x = 0; x < size.x; x++)
+		{
+			getBlockUnsafe(x, y) = map.getBlockUnsafe(x + start.x, y + start.y);
+		}
+	}
+}
+
+
+void pasteIntoMap(GameMap& map, Vector2 start);
--- src/gameLayer/structure.hpp
+++ src/gameLayer/structure.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
+#include <raylib.h>
 #include "blocks.hpp"
+#include "gameMap.hpp"
 
 struct Structure
 {
@@ -13,4 +15,7 @@ struct Structure
 
 	Block& getBlockUnsafe(int x, int y);
 	Block* getBlockSafe(int x, int y);
+
+	void copyFromMap(GameMap &map, Vector2 start, Vector2 end);
+	void pasteIntoMap(GameMap& map, Vector2 start);
 };

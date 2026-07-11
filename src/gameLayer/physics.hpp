3d5d291c9b1bbfeede2feb4442cd26941d6a8469
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-07-10 20:40:12 -0500

    added Transform2D struct

--- src/gameLayer/physics.hpp
+++ src/gameLayer/physics.hpp
@@ -1,2 +1,19 @@
 #pragma once
+#include <raylib.h>
 
+struct Transform2D
+{
+	Vector2 pos = {}; // center
+	float w = 0;
+	float h = 0;
+
+	Vector2 getCenter()			const { return { pos.x, pos.y }; }
+	Vector2 getTop()			const { return { pos.x, pos.y - h * 0.5f }; }
+	Vector2 getBottom()			const { return { pos.x, pos.y + h * 0.5f}; }
+	Vector2 getLeft()			const { return { pos.x - w * 0.5f, pos.y }; }
+	Vector2 getRight()			const { return { pos.x + w * 0.5f, pos.y }; }
+	Vector2 getTopLeft()		const { return { pos.x - w * 0.5f, pos.y - h * 0.5f }; }
+	Vector2 getTopRight()		const { return { pos.x + w * 0.5f, pos.y - h * 0.5f }; }
+	Vector2 getBottomLeft()		const { return { pos.x - w * 0.5f, pos.y + h * 0.5f }; }
+	Vector2 getBottomRight()	const { return { pos.x + w * 0.5f, pos.y + h * 0.5f }; }
+};
